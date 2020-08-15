c4f84c953332e744060f0457271052001741d864
wenq97 <wenq97@uw.edu>
2020-08-15 09:54:12 -0700

    Added basic routing functionality

---
+++ src/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+export default function Login() {
+    return (
+        <h1>This is the login page</h1>
+    );
+}
---
+++ src/service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+export default function ServiceDashboard() {
+    return (
+      <div>
+        <h1>This is the service dashboard</h1>  
+      </div>
+    );
+}
